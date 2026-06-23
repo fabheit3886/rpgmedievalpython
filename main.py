94b46d69567f9233889a1746ce04ecdb0c243d21
Fábio Heitor Bezerra Pires <20251041110004@ifrn.local>
2026-06-23 18:08:39 -0300

    mudanças bruscas

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from personagens.criador_personagens import CriadorPersonagem
 from mundo.historia import Historia
 
-
 criador = CriadorPersonagem()
 
 jogador = criador.criar()
 
-historia = Historia(
-    jogador
-)
+historia = Historia(jogador)
 
 historia.iniciar()
